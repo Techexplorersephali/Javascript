76537ea662a1cc492755ec1892f2c8728ad9089e
Techexplorersephali <138424988+Techexplorersephali@users.noreply.github.com>
2025-04-30 04:37:49 +0000

    basic

---
+++ test.js
@@ -0,0 +1,20 @@
+/*const accountId = 144553 
+let accountEmail= "sephali.3@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 TypeError: Assignment to constant variable.
+// console.log(accountId); 
+ accountEmail = "dhsisn@ndi"
+ accountPassword = "23444"
+ accountCity = "pune"
+
+ console.table([accountId,accountEmail, accountPassword, accountCity, accountState])
+
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+
