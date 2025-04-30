a310e0c43e47e7c23aafd5d0d15cd20b4c725ff4
Techexplorersephali <138424988+Techexplorersephali@users.noreply.github.com>
2025-04-30 04:37:15 +0000

    Data types

---
+++ 02.datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat js code as newer version of js
+
+// alert(3 + 3) we are using node.js not browser 
+
+// console.log(3+3); console.log("shifu") code readability is less 
+ // readability should be high 
+
+let name = "sephali"
+let age = 21
+let isLoggedIn= false 
+let state = null 
+
+
+// number => 2 to power 53 
+// bigint
+// string single quotes, double quotes "" ''
+//boolean => true/ false 0 or 1 
+//null => standalone value 
+// undefined => no value assigned
+// symbol =  uniquness 
+// object 
+// type of null is object and type of undefined if undefined
+// console.log(typeof )
