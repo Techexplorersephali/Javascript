842d4ade8b535046cdf37841efe5765028bd032b
Techexplorersephali <138424988+Techexplorersephali@users.noreply.github.com>
2025-05-01 05:05:45 +0000

    new chnages

--- .devcontainer/chapters /03_conversionoperation.js
+++ .devcontainer/chapters /03_conversionoperation.js
@@ -1,4 +1,4 @@
-let score = "33" 
+/*let score = "33" 
 
 console.log(typeof score);
 console.log(typeof(score)); //string
@@ -30,6 +30,54 @@ console.log(booleanIsLoggedIn); // true
 // "hitest" =. true 
 let someNumber = 33
 let stringNumber = String(someNumber) 
-console.log( typeof stringNumber) //string
+console.log( typeof stringNumber) //string */
 
+
+// ****************** OPERATIONS **************
+
+/*let value = 3
+let megValue= -value // -3
+console.log(negValue); // assign negative value */
+
+
+// basic maths operation 
+/*console.log(2+2);
+console.log(2-2);
+console.log(2**2); //exponent 
+
+
+
+ let str1 = "hello"
+ let str2 =" shifu" // spaceis given 
+ let str3= str1 + str2 
+ console.log(str3) // hello shifu 
+ // (with space because we have space in str2)
+
+
+
+console.log("1" + 2); // 12
+console.log(1 + "2") //12 
+console.log("1" +2 + 2) // 122, 
+// string at first so everything is treated as string  
+console.log( 1+ 2+ "2"); //32
+// string at last so no conversion of 1st two numbers will take place
+console.log(3 +4 * 5 % 6); // very confusing 
+console.log((3 +4) * 5 % 6); // use parenthesis 
+
+
+
+console.log(+true); //confusion //1 , increment is done so conversion happens
+console.log(+""); // 0 
+
+let num1, num2, num3 
+num1 = num2 = num3 = 2 + 2 // very bad code
+
+
+let gameCounter= 100 
+gamecounter++;
+console.log(gameCounter); // 101
+++ gameCounter; 
+console.log(gameCounter); // 101 
+
+*/
 
